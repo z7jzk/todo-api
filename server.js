16df82754af98160e9ae29127d0c989c6848c163
kyburzjz <kyburzjz@gmail.com>
2016-08-22 02:41:52 +0000

    Add PUT /todos/:id

--- server.js
+++ server.js
@@ -61,6 +61,34 @@ app.delete('/todos/:id', function (req, res) {
   }
 });
 
+app.put('/todos/:id', function (req, res) {
+  var todoId = parseInt(req.params.id, 10);
+  var item = _.findWhere(todos, {id: todoId});
+  var body = _.pick(req.body, 'description', 'completed');
+  var validAttributes = {};
+  
+  if (!item) {
+    return res.status(404).send();
+  }
+  
+  if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+    validAttributes.completed = body.completed;
+  } else if (body.hasOwnProperty('completed')) {
+    return res.status(400).send();
+  }
+  
+  if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length() > 0) {
+    validAttributes.description = body.description.trim();
+  } else if (body.hasOwnProperty('description')) {
+    return res.status(400).send();
+  }
+  
+  _.extend(item, validAttributes);
+  
+  res.json(item);
+  
+});
+
 app.listen(PORT, function () {
   console.log('Express listening on port ' + PORT + '!');
 })
