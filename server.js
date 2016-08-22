3576a043363647238d6056317e46ec9f4791ac43
kyburzjz <kyburzjz@gmail.com>
2016-08-22 03:13:51 +0000

    Added completed filter

--- server.js
+++ server.js
@@ -12,10 +12,20 @@ app.get('/', function (req, res) {
   res.send('Todo API Root');
 });
 
-// GET /todos
+// GET /todos?completed=true
 app.get('/todos', function (req, res) {
-  res.json(todos);
+  var queryParams = req.query;
+  var filteredTodos = todos;
+  
+  if (queryParams.hasOwnProperty('completed') && queryParams.completed == 'true') {
+    filteredTodos = _.where(filteredTodos, {completed: true});
+  } else if (queryParams.hasOwnProperty('completed') && queryParams.completed == 'false') {
+    filteredTodos = _.where(filteredTodos, {completed: false});
+  }
+  
+  res.json(filteredTodos);
 });
+
 // GET /todos/:id
 app.get('/todos/:id', function (req, res) {
   var todoId = parseInt(req.params.id, 10);
@@ -28,7 +38,7 @@ app.get('/todos/:id', function (req, res) {
   }
 });
 
-// POST
+// POST todos
 app.post('/todos', function (req, res) {
   var body = _.pick(req.body, 'description', 'completed');
   
@@ -48,6 +58,7 @@ app.post('/todos', function (req, res) {
   res.json(body);
 });
 
+// DELETE todos
 app.delete('/todos/:id', function (req, res) {
   var todoId = parseInt(req.params.id, 10);
   var item = _.findWhere(todos, {id: todoId});
@@ -61,6 +72,7 @@ app.delete('/todos/:id', function (req, res) {
   }
 });
 
+// UPDATE todos
 app.put('/todos/:id', function (req, res) {
   var todoId = parseInt(req.params.id, 10);
   var item = _.findWhere(todos, {id: todoId});
