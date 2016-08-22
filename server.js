fe51128c4f42aed33b761b847512775d4e9b3a82
kyburzjz <kyburzjz@gmail.com>
2016-08-22 03:35:45 +0000

    final static todos

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', function (req, res) {
   res.send('Todo API Root');
 });
 
-// GET /todos?completed=true
+// GET /todos?completed=true&q=house
 app.get('/todos', function (req, res) {
   var queryParams = req.query;
   var filteredTodos = todos;
@@ -22,6 +22,12 @@ app.get('/todos', function (req, res) {
   } else if (queryParams.hasOwnProperty('completed') && queryParams.completed == 'false') {
     filteredTodos = _.where(filteredTodos, {completed: false});
   }
+  
+  if (queryParams.hasOwnProperty('q') && queryParams.q.trim().length > 0) {
+    filteredTodos = _.filter(filteredTodos, function (todo) {
+      return (todo).description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+    });
+  } 
   
   res.json(filteredTodos);
 });
